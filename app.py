cd6ea558ef8969e78f39b89a81301a21181c0990
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2021-03-07 22:47:41 +0200

    Basic scraper and % calculator

---
+++ app.py
@@ -0,0 +1,33 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+headlines = []
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+        self.headlines = []
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            self.headlines.append(url)
+    def calculate_precentage(self, topics):
+        num = 0
+        topics = ["corona", "pandemic"]
+        for i in self.headlines:
+            for topic in topics:
+                if topic in i:
+                    num += 1
+
+        precentage = (num / len(self.headlines)) * 100
+        print(str(format(precentage, ".2f")) + "%")
+           
+scrape = Scraper('https://www.nytimes.com/international/')
+scrape.scrape()
+test = ["corona", "pandemic"]
+scrape.calculate_precentage(test)
